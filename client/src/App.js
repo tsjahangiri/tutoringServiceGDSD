a5ed2982a089b1b71614cb0a8e0bdd2bae8afc4c
Mohammad Rakibul Hasan <rkhasan333@gmail.com>
2021-11-02 18:02:46 +0100

    bootsrap button added

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,12 @@ function App() {
         >
           Learn React
         </a>
+
+        <button type="button" class="btn btn-primary">Primary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+
       </header>
     </div>
   );
