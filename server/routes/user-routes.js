b56dd5ec68ce267c59705624d4f096f7fa669640
SalmanHaydar <salmanhaydar3@gmail.com>
2021-12-14 00:38:16 +0100

    Milestone 2 demo project was implemented

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -4,17 +4,22 @@ const router = express.Router()
 let userController= require('../controller/userController')
 let loginController = require('../controller/loginController')
 let auth=require("../middleware/auth")
+const searchController = require('../controller/searchController')
 
 
 //************Map the resources to it's appropriate controller here********** */
-router.get('/user',auth,userController.getUser);
-router.get("/user/:id",auth,userController.getUserById);
-router.post('/user',auth,userController.createUser);
-router.patch("/user/:id",auth,userController.updateUser);
-router.delete("/user/:id",auth,userController.deleteUser);
+// router.get('/user',auth,userController.getUser);
+// router.get("/user/:id",auth,userController.getUserById);
+// router.post('/user',auth,userController.createUser);
+// router.patch("/user/:id",auth,userController.updateUser);
+// router.delete("/user/:id",auth,userController.deleteUser);
 
-router.post('/register', loginController.registerUser);
-router.post('/login',loginController.loginUser)
+// router.post('/register', loginController.registerUser);
+// router.post('/login',loginController.loginUser)
+
+
+
+router.get('/search/tutor/',searchController.search);
 
 
 
