d8bbf6d338ffdbba60c700894d6daeca10ba12c0
SalmanHaydar <salmanhaydar3@gmail.com>
2021-12-18 22:17:24 +0100

    CORS and / directory was added

--- server/App.js
+++ server/App.js
@@ -1,14 +1,19 @@
 const express = require('express')
 var bodyParser = require('body-parser')
+const cors = require('cors');
 const userRoutes = require('./routes/user-routes')
-const app = express ()
+const app = express()
 require('dotenv').config()
  
 var jsonParser = bodyParser.json()
  
-
+app.use(cors());
 app.use(jsonParser);
 app.use('/api/',userRoutes)
+
+app.get("/", (req, res, next) => {
+  res.json({"Message":"It is running"});
+});
 
 app.use(function (req, res, next) {
     next(createError(404));
