8140797ea0105914a0d21074b736bcd6386b27d1
Mohammad Rakibul Hasan <rkhasan333@gmail.com>
2022-01-13 22:39:34 +0100

    css added for OfferCourse module

--- client/src/pages/offerCourse/OfferCourse.js
+++ client/src/pages/offerCourse/OfferCourse.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
 import {Link} from 'react-router-dom'
+import "./OfferCourse.css";
 
 function OfferCourse(props) {
   return (
