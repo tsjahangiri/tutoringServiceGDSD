a6b8433ef671f0c7f50a329b0e665a36102ff42a
amlan <chowdhury.amlan-barua@informatik.hs-fulda.de>
2022-01-16 20:56:34 +0100

    resolved in root.js

--- client/src/core/sagas/root.js
+++ client/src/core/sagas/root.js
@@ -3,23 +3,9 @@ import { all, fork } from "redux-saga/effects";
 import type { Saga } from "redux-saga";
 import tutorSaga from "./tutor";
 import userSaga from "./user";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import courseSaga from "./course"
-
-export default function* rootSaga(): Saga<void> {
-  yield all([fork(tutorSaga), fork(userSaga), fork(courseSaga)]);
-=======
-import studentShowSaga from "./studentShow"
-
-export default function* rootSaga(): Saga<void> {
-  yield all([fork(tutorSaga), fork(userSaga), fork(studentShowSaga)]);
->>>>>>> origin/frontend_hasib
-=======
 import courseSaga from "./course"
 import studentShowSaga from "./studentShow"
 
 export default function* rootSaga(): Saga<void> {
   yield all([fork(tutorSaga), fork(userSaga), fork(studentShowSaga), fork(courseSaga)]);
->>>>>>> origin/frontend_hasib
 }
