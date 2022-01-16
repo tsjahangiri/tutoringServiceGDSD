38495fe6b712a5745811e772dc1f274f2efb0b72
Mohammad Rakibul Hasan <rkhasan333@gmail.com>
2022-01-16 14:51:03 +0100

    api integration code to save course added

--- client/src/core/reducers/root.js
+++ client/src/core/reducers/root.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import tutor from "./tutor";
+import course from "./course"
 
 const rootReducer = combineReducers({
   tutor,
+  course,
 });
 
 export default rootReducer;
