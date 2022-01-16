e72f084ef807e1cbb86ba5ff3245fb1dc8bdebc3
Nisha Devi <nishadevi.bhojwani@gmail.com>
2022-01-16 12:41:09 +0100

    Merge Branch with Salman Branch Changes

--- server/App.js
+++ server/App.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 var bodyParser = require("body-parser");
-const routes = require("./routes");
+const routes = require("./routes/routes.js");
 const app = express();
 const port = 3000;
 require("dotenv").config();
