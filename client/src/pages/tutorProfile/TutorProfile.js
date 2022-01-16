fba868104df78348405259d65604191e3ed71808
amlan <chowdhury.amlan-barua@informatik.hs-fulda.de>
2022-01-16 02:20:57 +0100

    add qualification list to tutor profile page

--- client/src/pages/tutorProfile/TutorProfile.js
+++ client/src/pages/tutorProfile/TutorProfile.js
@@ -4,6 +4,9 @@ import { Container } from "react-bootstrap";
 import AboutMe from "./aboutme/AboutMe";
 import TutorProfileBody from "./tutorprofilebody/TutorProfileBody";
 import CourseList from "./courselist/CourseList";
+import ReviewList from "./reviewlist/ReviewList";
+import QualificationList from "./qualificationlist/QualificationList";
+import Page from "../../components/page/Page";
 // import FilterBar from "./filterBar/FilterBar";
 // import { fetchTutorList } from "../../../core/actionCreators/tutor";
 
@@ -15,6 +18,10 @@ export default function TutorProfile() {
       <br />
       <Container>
         <CourseList />
+        <br />
+        <QualificationList/>
+        <br />
+        <ReviewList />
         <br />
         {/* <QualificationList/>
           <br />
--- client/src/pages/tutorProfile/reviewlist/ReviewList.js
+++ client/src/pages/tutorProfile/reviewlist/ReviewList.js
@@ -2,20 +2,15 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Card, ListGroup, Row, Col, Container, Button, ListGroupItem, Badge, Pagination } from "react-bootstrap";
 import { div, img } from "bootstrap";
-import "./CourseList.css";
+import "./ReviewList.css";
 import Paging from "../../../components/paging/Paging";
 
-export default function CourseList() {
+export default function ReviewList() {
 
     // var data = useSelector(getCourseList); //TODO: Change var to const
 
     let active = 2;
     let items = [];
-
-    const photo = "/logo192.png";
-    const name = "Amlan Chowdhury";
-    const about = "Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.";
-
     const viewDetails = () => {
         var newFilters = {};
     };
@@ -23,13 +18,13 @@ export default function CourseList() {
     var data = [
         {
             id: 1,
-            SubjectName: "Distributed Applications",
-            rate: 36
+            name: "Hasib Iqbal",
+            text: "Some quick example text to build on the card title and make up the bulk of the card's content"
         },
         {
             id: 2,
-            SubjectName: "Cloud Computing",
-            rate: 30
+            name: "Amlan Chowdhury",
+            text: "Some quick example text to build on the card title and make up the bulk of the card's content"
         }
     ];
 
@@ -54,12 +49,9 @@ export default function CourseList() {
                         className="d-flex justify-content-between align-items-start"
                     >
                         <div className="ms-2 me-auto">
-                            <div className="fw-bold">{item.SubjectName}</div>
-                            <Button variant="primary" type="submit" onClick={viewDetails}> Details </Button>
+                            <div className="fw-bold">{item.name}</div>
+                            <div className="fw-light">{item.text}</div>
                         </div>
-                        <Badge variant="primary" pill>
-                            {`$${item.rate}/hr`}
-                        </Badge>
                     </ListGroup.Item>;
                 })}
             </ListGroup>
