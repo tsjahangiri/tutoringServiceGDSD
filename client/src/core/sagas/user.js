9e958f04bbf2c661b69f2cc6721ba4de8ccf09fa
Rohat Sagar <rohataliassonusagar@gmail.com>
2022-01-17 16:32:34 +0100

    Added code for registration integration

--- client/src/core/sagas/user.js
+++ client/src/core/sagas/user.js
@@ -3,12 +3,17 @@ import { takeEvery, call, put } from "redux-saga/effects";
 import type { Saga } from "redux-saga";
 import jwt_decode from "jwt-decode";
 import { executeApiCall } from "./api";
-import { loginApi } from "../endpoints";
-import { LOGIN_USER } from "../actionTypes/user";
-import { setCurrentUser, setLoginError } from "../actionCreators/user";
+import { loginApi, registerApi } from "../endpoints";
+import { LOGIN_USER, REGISTER_USER } from "../actionTypes/user";
+import {
+  setCurrentUser,
+  setLoginAlert,
+  setRegistrationAlert,
+} from "../actionCreators/user";
 
 export default function* loginSaga(): Saga<void> {
   yield takeEvery(LOGIN_USER, login);
+  yield takeEvery(REGISTER_USER, register);
 }
 
 export function* login(action: Object): Saga<void> {
@@ -28,8 +33,6 @@ export function* login(action: Object): Saga<void> {
 
   const { isSuccessful, response = {} } = apiResponse;
 
-  console.log(response);
-
   if (isSuccessful) {
     const { email, token } = response;
     if (token !== undefined) {
@@ -37,8 +40,30 @@ export function* login(action: Object): Saga<void> {
       const { exp, status, user_name, user_type } = decoded;
       yield put(setCurrentUser({ email, user_name, user_type, token, exp }));
     }
+  } else {
+    const errorMessage = response.ErrorMessage || response.message;
+    yield put(setLoginAlert(errorMessage));
   }
+}
 
-  const errorMessage = response.ErrorMessage || response.message;
-  yield put(setLoginError(errorMessage));
+export function* register(action: Object): Saga<void> {
+  const apiOptions: ApiOptions = {
+    url: registerApi,
+    method: "POST",
+    params: action.payload.data,
+    useJwtSecret: false,
+  };
+
+  const apiResponse: ApiResponse = yield call(executeApiCall, apiOptions);
+
+  const { isSuccessful, response = {} } = apiResponse;
+
+  if (isSuccessful) {
+    const { message } = response;
+    action.payload.navigate("/login");
+    yield put(setLoginAlert(message, "success"));
+  } else {
+    const errorMessage = response.ErrorMessage || response.message;
+    yield put(setRegistrationAlert(errorMessage));
+  }
 }
