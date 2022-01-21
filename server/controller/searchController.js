24785b542c2088e92dfb994d119ff952178d9eb8
amlan <chowdhury.amlan-barua@informatik.hs-fulda.de>
2022-01-21 11:06:23 +0100

    resolved conflict

--- server/controller/searchController.js
+++ server/controller/searchController.js
@@ -26,7 +26,7 @@ module.exports={
         }
 
         console.log(query);
-y
+
         database.query(query, function (err, result, fields) {
             if (err) throw err;
             res.json(result);
