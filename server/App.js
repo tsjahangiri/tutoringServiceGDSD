2ecfd77e191cd26f7747e2fa73f97340000e112b
amlan <chowdhury.amlan-barua@informatik.hs-fulda.de>
2022-01-21 11:16:47 +0100

    changes

--- server/App.js
+++ server/App.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
     next();
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Help Me Learn API listening at http://localhost:${port}`);
 });
 
